acc9fcd042fe2a211b7c57ae9864c82e1eb6c8fd
eunbaming <dbskqnsghd@naver.com>
2023-10-27 17:22:33 +0900

    feat: Implement the Home section to be transparent when scrolling down

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
 const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
+// 또는 header.offsetHeight;
 console.log(headerHeight);
 
 document.addEventListener("scroll", () => {
@@ -9,4 +10,13 @@ document.addEventListener("scroll", () => {
   } else {
     header.classList.remove("header--dark");
   }
+});
+
+// Home 섹션을 아래로 스크롤 시 투명하게 처리
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+console.log("homeHeight", homeHeight);
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
