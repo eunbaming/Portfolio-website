b79e244307a120a4e6c5850ba304fe5a1ba71eb3
eunbaming <dbskqnsghd@naver.com>
2023-10-31 14:52:03 +0900

    feat: Filter projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
+
+document.addEventListener("click", (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return; // 함수 종료
+  }
+
+  // Active 메뉴 재설정
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  event.target.classList.add("category--selected");
+
+  // 프로젝트 필터링
+  projectsContainer.classList.add("anim-out");
+  projects.forEach((project) => {
+    if (filter == "all" || filter == project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 200);
+});
